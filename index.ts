56ad41027333f4d5c33c73dc619f90088cf2bc70
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2023-10-12 16:20:33 -0400

    4.3 extract method drawTile

--- index.ts
+++ index.ts
@@ -429,12 +429,16 @@ function draw() {
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
+      drawTile(g, y, x);
+    }
+  }
+}
+
+function drawTile(g: CanvasRenderingContext2D, y: number, x: number) {
       map[y][x].color(g)
 
       if (!map[y][x].isAir() && !map[y][x].isPlayer())
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-        }
-  }
 }
 
 function drawPlayer(g: CanvasRenderingContext2D) {
